f73ba2e21532505e13c2f4982358d1495df704cd
Wilson Neto <contato@wilsonneto.com.br>
2024-06-16 16:37:04 -0300

    Options Patterns

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -5,8 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.Configure<OpenAiSettings>(
-    builder.Configuration.GetSection("Providers:OpenAI"));
+builder.Services.AddSingleton<IValidateOptions<OpenAiSettings>, OpenAiSettingsValidate>();
+
+builder.Services.AddOptionsWithValidateOnStart<OpenAiSettings>()
+    .Bind(builder.Configuration.GetSection("Providers:OpenAI"));
 
 builder.Services.AddTransient<OpenAiGateway>();
 
@@ -26,21 +28,34 @@ app.Run();
 public record EndpointInput(string Prompt);
 public record EndpointOutput(string Response);
 
-
 internal class OpenAiGateway(IOptions<OpenAiSettings> openAiSettings)
 {
-    private readonly string _baseAddress = openAiSettings.Value.BaseAddress
-        ?? throw new ArgumentException("Base Address not found...");
-
-    private readonly string _apiKey = openAiSettings.Value.ApiKey
-        ?? throw new ArgumentException("Api key not found...");
+    private readonly string _baseAddress = openAiSettings.Value.BaseAddress;
+    private readonly string _apiKey = openAiSettings.Value.ApiKey;
     
     public Task<string> ExecutePrompt(string prompt) => 
         Task.FromResult($"BaseAddress: {_baseAddress} / Api Key: {_apiKey}");
 }
 
+public class OpenAiSettingsValidate : IValidateOptions<OpenAiSettings>
+{
+    public ValidateOptionsResult Validate(string? name, OpenAiSettings options)
+    {
+        var errors = new List<string>(); 
+        if(string.IsNullOrWhiteSpace(options.BaseAddress))
+            errors.Add("BaseAddress not found");
+        if(string.IsNullOrWhiteSpace(options.ApiKey))
+            errors.Add("ApiKey not found");
+
+        return errors.Count > 0 ? 
+            ValidateOptionsResult.Fail(string.Join(';', errors)) 
+            : ValidateOptionsResult.Success;
+    }
+}
+
 public class OpenAiSettings
 {
-    public string? BaseAddress { get; set; }
-    public string? ApiKey { get; set; }
+    public string BaseAddress { get; init; } = "";
+    
+    public string ApiKey { get; init; } = "";
 }
